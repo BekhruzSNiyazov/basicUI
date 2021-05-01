f62edac79989efb33b5deaa0555a4deb80e366c8
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-02 01:36:09 +0300

    finished navbar

--- src/basicUI.js
+++ src/basicUI.js
@@ -75,17 +75,6 @@ class navBar {
 		} else if (role === "button") {
 			li.innerHTML = `<button type="button" class="btn btn-${properties[0]} ${classes}" id="${id}">${properties[1]}</button>`
 		} else if (role === "dropdown") {
-			// <li class="nav-item dropdown">
-			//           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-			//             Dropdown
-			//           </a>
-			//           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-			//             <li><a class="dropdown-item" href="#">Action</a></li>
-			//             <li><a class="dropdown-item" href="#">Another action</a></li>
-			//             <li><hr class="dropdown-divider"></li>
-			//             <li><a class="dropdown-item" href="#">Something else here</a></li>
-			//           </ul>
-			//         </li>
 			li.className = "nav-item dropdown";
 			let a = document.createElement("a");
 			a.className = "nav-link dropdown-toggle";
