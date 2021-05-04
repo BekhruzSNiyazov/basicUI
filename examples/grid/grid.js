8565260c5e422724832389e7e1b4a77ec5104044
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-05-04 03:01:18 +0300

    grid

--- examples/grid/grid.js
+++ examples/grid/grid.js
@@ -8,23 +8,19 @@ setTitle("Grid - basicUI");
 
 addNewLine();
 
+// creating cards
 let card1 = new Card("Card One", "This is the first card.", ["Link", "#"]);
-card1.position = "center";
-card1.add(false);
 let card2 = new Card("Card Two", "This is the second card.", ["Link", "#"]);
-card2.position = "center";
-card2.add(false);
 let card3 = new Card("Card Three", "This is the third card.", ["Link", "#"]);
-card3.position = "center";
-card3.add(false);
 let card4 = new Card("Card Four", "This is the fourth card.", ["Link", "#"]);
-card4.position = "center";
-card4.add(false);
 let card5 = new Card("Card Five", "This is the fifth card.", ["Link", "#"]);
-card5.position = "center";
-card5.add(false);
 let card6 = new Card("Card Six", "This is the sixth card.", ["Link", "#"]);
-card6.position = "center";
-card6.add(false);
 
+let cards = [card1, card2, card3, card4, card5, card6];
+cards.forEach((card, index) => {
+	card.position = "center";
+	card.add(false);
+});
+
+// creating a grid
 createGrid([[card1, card2, card3], [card4, card5, card6]]);
