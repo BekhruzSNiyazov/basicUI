fd0656f944d70cab513618a67b895aff292ea114
BekhruzSNiyazov <bekhruzsniyazov@outlook.com>
2021-06-29 16:40:59 +0300

    fixed the navbar bug

--- src/basicUI.js
+++ src/basicUI.js
@@ -198,8 +198,8 @@ class NavBar extends basicUIObject {
 		this.src = `<div class="container-fluid">
                         <a class="navbar-brand" id="navbar-image" href="#" style="padding-top: 0;"></a>
                         <a class="navbar-brand" id="navbar-title" href="#">Navbar</a>
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
+                        <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <i class="fas fa-bars"></i>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="items-list"></ul>
